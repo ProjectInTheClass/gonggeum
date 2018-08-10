8df1d88c7d027819af766255893e27467191013e
hsy159 <whj0210@likelion.org>
2018-08-10 20:20:07 +0900

    having error dealing with load logs, table refresh, photo, align, icon, plist saving, need website

--- smartMoney/MoneyLog.swift
+++ smartMoney/MoneyLog.swift
@@ -10,11 +10,6 @@ import Foundation
 
 //initialize
 
-struct GroupInfo:Codable{
-    var name: String
-    var logs: [MoneyLog]
-}
-
 struct MoneyLog: Codable{
     var y: Int
     var m: Int
@@ -26,20 +21,12 @@ struct MoneyLog: Codable{
     //evidence info? image? string?
 }
 
+//GroupInfos = ["swift class" : [log1, log2, log3...]]
+var GroupInfos : [String : [MoneyLog]] = [:]
 
+var currentGroup : String?
+var currentGroupLogs :[MoneyLog] = []
 
-//var logs : [MoneyLog] = []
-var GroupInfos : [GroupInfo] = []
-
-
-//var currentGroup : String?
-//var currentGroupLogs :[MoneyLog] = []
-
-
-var currentGroup : String = "test group"
-var currentGroupLogs :[MoneyLog] = [MoneyLog(y:1, m:2, d: 3, eventName: "hi", money: 1, memo: "1", InOut: 1)]
-
-//var balanceInfo = 0
 
 //json
 let LogJsonPath = NSHomeDirectory() + "/Documents/moneyLog.json"
@@ -63,15 +50,22 @@ func loadLog(){
     print(currentGroup)
     print(currentGroupLogs)
     
-    if let data = try? Data.init(contentsOf: LogJsonUrl), let infosFromFile = try? decoder.decode([GroupInfo].self, from: data){
-        print("개수 : ", infosFromFile.count)
+    
+    
+    if let infosFromFile = NSDictionary(contentsOfFile: LogJsonPath) as? [String:[MoneyLog]]{
+        GroupInfos = infosFromFile
+        
         if infosFromFile.count > 0 {
             GroupInfos = infosFromFile
-            currentGroupLogs = infosFromFile[0].logs
+            currentGroup = GroupInfos.Keys[0]
         }
+            
         else {
-            currentGroup = "test group"
-            currentGroupLogs = [MoneyLog(y:1, m:2, d: 3, eventName: "hi", money: 1, memo: "1", InOut: 1)]
+            GroupInfos = ["swift study" : [MoneyLog(y:1, m:2, d: 3, eventName: "hi", money: 1, memo: "1", InOut: 1)], "swift study2" : [MoneyLog(y:1, m:2, d: 3, eventName: "hi", money: 1, memo: "1", InOut: 1)]]
+            currentGroup = "swift study"
+            currentGroupLogs = GroupInfos["swift study"]!
+            
+            
         }
         
         //initialize current Group to first group in JSON file
@@ -79,13 +73,38 @@ func loadLog(){
         print("current Group : ", currentGroup)
         print("current Group Logs : ", currentGroupLogs)
     }
-    
+    /*
+    if let data = try? Data.init(contentsOf: LogJsonUrl), let infosFromFile = try? decoder.decode(GroupInfos.self, from: data){
+        print("개수 : ", infosFromFile.count)
+        
+        if infosFromFile.count > 0 {
+            GroupInfos = infosFromFile
+            currentGroupLogs = infosFromFile[0].logs
+        }
+        else {
+            GroupInfos = ["swift study" : [MoneyLog(y:1, m:2, d: 3, eventName: "hi", money: 1, memo: "1", InOut: 1)], "swift study2" : [MoneyLog(y:1, m:2, d: 3, eventName: "hi", money: 1, memo: "1", InOut: 1)]]
+            currentGroup = "swift study"
+            currentGroupLogs = GroupInfos["swift study"]!
+
+        
+        }
+        
+        //initialize current Group to first group in JSON file
+        //currentGroup = infosFromFile[0].name
+        print("current Group : ", currentGroup)
+        print("current Group Logs : ", currentGroupLogs)
+    }
+    */
     // plist
     
 }
 
 func saveLog(){
     // logs를 파일로 저장
+    
+    
+    
+    /*
     let encoder = JSONEncoder()
     
     if let data = try? encoder.encode(GroupInfos) {
@@ -93,6 +112,8 @@ func saveLog(){
             try! data.write(to: LogJsonUrl)
             print("saving logs[] to JSON file finished!")
         }
+    
+    */
     
     
     // plist 저장
--- smartMoney/myGroupViewController.swift
+++ smartMoney/myGroupViewController.swift
@@ -45,9 +45,9 @@ class myGroupViewController: UIViewController, UITableViewDataSource {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        if let indexPath = self.groupsTable.indexPathForSelectedRow, let nextVC = segue.destination as? moneyLogViewController{
+        if let indexPath = self.groupsTable.indexPathForSelectedRow{
             let selectedData = grouplist[indexPath.row].title
-            nextVC.data = selectedData
+            currentGroup = selectedData
         }
  
     }
